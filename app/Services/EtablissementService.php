cc2981e3afc63137fdd36c0a54f2773ad5815738
MeriemKhadim <khadim.meriem@gmail.com>
2021-12-21 09:32:26 +0100

    Fix typo

--- app/Services/EtablissementService.php
+++ app/Services/EtablissementService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-interface EtablissementService{
+interface EtablissementService
+{
     public function findAll();
-   
 }
--- resources/views/admin/edit_profile.blade.php
+++ resources/views/admin/edit_profile.blade.php
@@ -10,6 +10,7 @@
         </div>
     </div>
     <div class="row">
+        
         <div class="col-12 col-md-6 col-lg-6">
             <div class="card">
                 <form method="post" class="needs-validation" novalidate="">
